c7ab7ca7fd3ef371036425dce30dcb2580d95e2b
Tiago Rocha <tiagoferreira.info@gmail.com>
2025-04-19 14:04:14 -0300

    router

---
+++ router-exemplo/analysis/__init__.py
@@ -0,0 +1,5 @@
+# analysis/__init__.py
+
+from analysis.financial_analysis import execute_financial_analysis_completion, execute_financial_analysis_chat
+
+__all__ = ["execute_financial_analysis_completion", "execute_financial_analysis_chat"]
---
+++ router-exemplo/llm_service/__init__.py
@@ -0,0 +1,4 @@
+# llm_service/__init__.py
+from .llm_service import LLMService
+
+__all__ = ["LLMService"]
